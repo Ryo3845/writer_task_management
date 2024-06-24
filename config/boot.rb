8ae07cb3abc7f06bd95b9046553f3ec31b74c358
Ryo3845 <ogaryo.writer@gmail.com>
2024-06-25 01:23:11 +0900

    Create static-pages

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,6 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+#rails serever起動に障害を起こすため、bootsnapをコメントアウト
+# require "bootsnap/setup" # Speed up boot time by caching expensive operations.
