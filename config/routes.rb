11a4c67af35d664278e8afc6f1b8dd3a3e1258a6
Ryo3845 <ogaryo.writer@gmail.com>
2024-06-30 22:36:29 +0900

    Create user data in the User model

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get "/function", to: "static_pages#function"
   get "/pricing", to: "static_pages#pricing"
   get "/signup", to: "users#new"
+  resources :users
 end
