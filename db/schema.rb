6ecdf5b2754dd333f52ece81cb79b5061157d29b
Ryo3845 <ogaryo.writer@gmail.com>
2024-08-07 01:35:28 +0900

    Narrow down to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -1,55 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema[7.0].define(version: 2024_07_03_153927) do
-  create_table "attachments", force: :cascade do |t|
-    t.integer "project_id", null: false
-    t.string "description"
-    t.string "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["project_id"], name: "index_attachments_on_project_id"
-  end
-
-  create_table "clients", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "deadlines", force: :cascade do |t|
-    t.integer "project_id", null: false
-    t.date "due_date"
-    t.string "description"
-    t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["project_id"], name: "index_deadlines_on_project_id"
-  end
-
-  create_table "projects", force: :cascade do |t|
-    t.string "title"
-    t.integer "client_id", null: false
-    t.decimal "compensation"
-    t.date "final_deadline"
-    t.float "target_working_hours"
-    t.float "actual_working_hours"
-    t.decimal "hourly_rate"
-    t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["client_id"], name: "index_projects_on_client_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -58,8 +7,4 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_03_153927) do
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
-  add_foreign_key "attachments", "projects"
-  add_foreign_key "deadlines", "projects"
-  add_foreign_key "projects", "clients"
 end
