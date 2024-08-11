fe50fd61c9cab60c33a8016a38f185809f4d632f
Ryo3845 <ogaryo.writer@gmail.com>
2024-08-11 22:55:08 +0900

    Complete the hamburger menu for PC

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,12 +1,26 @@
-'use strict'
-console.log("JavaScriptファイルは読み込まれています")
-document.addEventListener('turbo:load', () => {
-  const openMenu = document.getElementById('open-menu')
-  const overlay = document.getElementById('overlay')
-  const closeMenu = document.getElementById('close-menu')
+'use strict';
 
-  openMenu.addEventListener('click', () => {
-    console.log("三本線メニューはクリックされています")
-    overlay.classList.add('show-menu');
-  })
-});
+{
+  // ファイルの読み込み確認
+  console.log("JavaScriptファイルは読み込まれています");
+
+  document.addEventListener('turbo:load', () => {
+    const mainContens = document.getElementById('main-content');
+    const openMenu = document.getElementById('open-menu');
+    const overlay = document.getElementById('overlay');
+  
+    openMenu.addEventListener('click', (e) => {
+      // 三本線メニュークリック確認
+      console.log("三本線メニューはクリックされています");
+      overlay.classList.toggle('show-menu');
+      e.stopPropagation(); // メニューボタンのクリックがドキュメントに伝播するのを防ぐ
+    });
+
+    // ドキュメント全体にイベントリスナーを追加
+    document.addEventListener('click', (e) => {
+      if(overlay.classList.contains('show-menu') && !overlay.contains(e.target) && e.target !== openMenu) {
+        overlay.classList.remove('show-menu');
+      }
+    });
+  });
+}
