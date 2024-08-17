3e9031a5712abb7d509670af8834d90389203c37
Ryo3845 <ogaryo.writer@gmail.com>
2024-08-18 03:03:22 +0900

    Create the advanced-login branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_08_125828) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
+ActiveRecord::Schema[7.0].define(version: 2024_08_17_180155) do
   create_table "projects", force: :cascade do |t|
+    t.integer "user_id", null: false
     t.string "client"
     t.string "title"
     t.integer "status"
+    t.string "other_task"
     t.date "task_deadline"
     t.date "final_deadline"
     t.boolean "final_deadline_not_applicable"
@@ -28,17 +27,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_08_125828) do
     t.decimal "total_working_hours", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "other_task"
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
+    t.string "remember_digest"
   end
 
   add_foreign_key "projects", "users"
