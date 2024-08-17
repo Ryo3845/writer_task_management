5d48f2f496dc334f290dbaa9270227c484e49e1f
Ryo3845 <ogaryo.writer@gmail.com>
2024-08-18 00:18:00 +0900

    Improved migration issues to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_08_08_125828) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string "client"
     t.string "title"
@@ -26,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_08_125828) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "other_task"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
